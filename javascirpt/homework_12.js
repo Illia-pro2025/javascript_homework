982d17ed5286fbb5631129a495a55013d0da7601
Illia-pro2025 <illamyrovec05@gmail.com>
2026-04-23 19:30:47 +0200

    HW 12

--- javascirpt/homework_12.js
+++ javascirpt/homework_12.js
@@ -1,48 +1,103 @@
 // Завдання 1
-// Напиши скрипт, який, для об'єкта user, послідовно:
-// додає поле mood зі значенням 'happy'
-// замінює значення hobby на 'skydiving'
-// замінює значення premium на false
-// виводить вміст об'єкта user в форматі ключ:значення
-//  використовуючи Object.keys() і for...of
-const user={
-    myPasion:"hobby",
-    subscript:"premium"
-}
- const keys =Object.keys(user)
- for (const key of keys) {
-    user.key=value
-    // console.log(user.myPasion,"skydiving");
- }
-// Завдання 2
-// Напиши функцію countProps(obj), яка рахує кількість
-//  властивостей в об'єкті. Функція повертає число
-//  — кількість властивостей.
 
+const user = {
+  myPasion: "hobby",
+  subscript: "premium",
+};
+user.mood = "happy";
+user.myPasion = "skydiving";
+user.subscript = "false";
+const keys = Object.keys(user);
+console.log(user);
+
+// Завдання 2
+
+const countProps = function (obj) {
+  let sum = [];
+  const key = Object.keys(obj);
+  sum.push(key.length);
+  return sum;
+};
+console.log(countProps(user));
 // Завдання 3
-// Напиши функцію findBestEmployee(employees), яка
-//  приймає об'єкт співробітників і повертає ім'я
-// найпродуктивнішого (який виконав більше всіх задач)
-// . Співробітники і кількість виконаних завдань містяться
-//  як властивості об'єкта в форматі "ім'я":"кількість задач".
+
+const employees = {
+  Іван: 10,
+  Марія: 15,
+  Олег: 7,
+  Анна: 20,
+};
+
+const findBestEmployee = function (employees) {
+  let bestname = "";
+  let maxNumber = 0;
+  for (let name in employees) {
+    if (employees[name] > maxNumber) {
+      maxNumber = employees[name];
+      bestname = name;
+    }
+  }
+  return `${bestname}:${maxNumber}`;
+};
+
+console.log(findBestEmployee(employees));
 
 // Завдання 4
-// Напиши функцію countTotalSalary(employees) приймаючу об'єкт
-//  зарплат. Функція рахує загальну суму зарплати працівників
-// і повертає її. Кожне поле об'єкта, переданого в функцію, має
-//  вигляд "ім'я":"зарплата".
+
+const employeesSalary = {
+  Іван: 1000,
+  Марія: 1500,
+  Олег: 1200,
+  Анна: 2000,
+};
+
+function countTotalSalary(employees) {
+  let total = 0;
+
+  for (let name in employees) {
+    total += employees[name];
+  }
+
+  return total;
+}
+
+console.log(countTotalSalary(employeesSalary));
 
 // Завдання 5
-// Напиши функцію getAllPropValues(arr, prop), яка отримує масив
-//  об'єктів і ім'я властивості. Повертає масив значень певної
-// властивості prop з кожного об'єкта в масиві.
+
+const products = [
+  { name: "Apple", price: 30 },
+  { name: "Banana", price: 20 },
+  { name: "Orange", price: 25 },
+];
+const openProducts = function (products) {
+  let productsName = [];
+  let productsPrice = [];
+  for (const item of products) {
+    productsName.push(item.name);
+    productsPrice.push(item.price);
+  }
+ return productsName
+
+};
+console.log(openProducts(products));
 
 // Завдання 6
-// Напиши функцію calculateTotalPrice(allProdcuts, productName),
-//  яка отримує масив об'єктів та ім'я продукту (значення властивості
-//  name). Повертає загальну вартість продукту (ціна * кількість).
 
-// Викличи функції для перевірки працездатності твоєї реалізації.
+const allProdcuts = [
+  { quanty: 10, price: 88, name: "Apple" },
+  { quanty: 50, price: 5, name: "Banana" },
+  { quanty: 80, price: 29, name: "Orange" },
+];
+
+const calculateTotalPrice = function (allProdcuts) {
+  let string = "";
+  for (const item of allProdcuts) {
+    string += `name:${item.name},price:${item.quanty * item.price}`;
+  }
+  return string;
+};
+console.log(calculateTotalPrice(allProdcuts));
 
 // Завдання 7 — додаткове, виконувати не обов'язково
 // Напиши сценарій керування особистим кабінетом інтернет-банку.
